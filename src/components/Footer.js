c581aec4cc54cfd98375209df7f2dab320bf78cd
omkar kulkarni <124461142+omkarkulkarni1632@users.noreply.github.com>
2023-10-28 18:36:30 +0530

    LAST

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -53,7 +53,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.instagram.com/animeshjha1906/"
+                href="https://www.instagram.com/omkarkulkarni1632/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
